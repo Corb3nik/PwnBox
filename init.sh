b18538efdbc7207ef8e15e1420044bc0b0962e21
Corb3nik <ian@subnet192.com>
2017-10-27 02:17:43 -0400

    Adds qemu and openssl for ARM

--- init.sh
+++ init.sh
@@ -6,6 +6,7 @@ mkdir ~/tools
 cd ~/tools
 
 # Updates
+sudo dpkg --add-architecture i386
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
@@ -16,7 +17,17 @@ sudo apt-get -y install python3-pip python-pip
 sudo apt-get -y install ipython
 sudo apt-get -y install wget
 sudo apt-get -y install git
+sudo apt-get -y install cmake
 
+# Libs
+sudo apt-get -y install libc6:i386 libncurses5:i386 libstdc++6:i386
+sudo apt-get -y install libc6-dev libc6-dev-i386
+sudo apt-get -y install libc6-dbg libc6-dbg:i386
+sudo apt-get -y install libc6-armhf-armel-cross
+
+####
+# Reverse-Engineering & Pwning
+####
 # Angr
 sudo pip install angr
 
@@ -30,11 +41,29 @@ sudo pip install ropper
 cd ~/tools && git clone https://github.com/pwndbg/pwndbg
 cd pwndbg && ./setup.sh
 
-# Binwalk
-cd ~/tools && git clone https://github.com/devttys0/binwalk
-cd binwalk && sudo python setup.py install
-
 # Radare2
 cd ~/tools && git clone https://github.com/radare/radare2
 cd radare2 && ./sys/install.sh
 
+# libc Database
+cd ~/tools && git clone https://github.com/niklasb/libc-database
+
+# Cross Debugging & Compiling
+sudo apt-get -y install gcc-arm-linux-gnueabihf
+sudo apt-get -y install qemu qemu-user qemu-user-static
+sudo apt-get install 'binfmt*'
+sudo mkdir /etc/qemu-binfmt
+sudo ln -s /usr/arm-linux-gnueabihf /etc/qemu-binfmt/arm
+
+# OpenSSL 1.0.0 for ARM
+cd ~/tools && git clone https://github.com/openssl/openssl
+cd openssl && git checkout OpenSSL_1_0_0-stable
+./Configure linux-armv4 shared && make CC=arm-linux-gnueabihf-gcc
+sudo cp libssl.so* libcrypto.so* /etc/qemu-binfmt/arm/lib/
+
+####
+# Forensics/Steganography
+####
+# Binwalk
+cd ~/tools && git clone https://github.com/devttys0/binwalk
+cd binwalk && sudo python setup.py install
