a695cc24e3b94e76ff731db079103b6e720f3fd1
Corb3nik <ian@subnet192.com>
2017-12-12 02:40:55 -0500

    Add template pwntools scripts

---
+++ templates/remote.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python2
+
+from pwn import *
+
+# == How to use ==
+# python remote.py LOCAL
+# python remote.py REMOTE IP=127.0.0.1 PORT=1337
+
+settings = {
+
+    # Path to binary
+    "binary"        : "/bin/sh",
+
+    # Path to custom libc
+    "libc"          : None,
+}
+
+# Exploit here
+def exploit():
+
+    # Available variables
+    # p      => Tubes! (http://docs.pwntools.com/en/stable/tubes.html)
+    # binary => ELF of binary
+
+    p.interactive()
+
+
+# Initial setup
+if __name__  == "__main__":
+
+    binary = ELF(settings['binary'])
+    p = None
+
+    if settings['libc']:
+        binary.libc = ELF(settings['libc'])
+
+
+    if args['LOCAL']:
+        p = process(binary.path)
+
+    if args['REMOTE']:
+        if not all([args.IP, args.PORT]):
+            log.warning("Missing IP/PORT arguments.")
+            exit()
+
+        p = remote(args.IP, int(args.PORT))
+
+    if not p:
+        log.warning("Missing LOCAL/REMOTE argument.")
+        exit()
+
+    pause()
+    exploit()
